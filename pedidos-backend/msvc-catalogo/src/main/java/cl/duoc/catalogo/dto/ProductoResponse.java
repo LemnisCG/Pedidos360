15005b230878db068359cc61f77873a1074e2960
Alcindo Chavarria <139635361+onechavarria@users.noreply.github.com>
2026-09-08 01:03:26 -0300

    feat: implementar microservicio catalogo

---
+++ pedidos-backend/msvc-catalogo/src/main/java/cl/duoc/catalogo/dto/ProductoResponse.java
@@ -0,0 +1,25 @@
+package cl.duoc.catalogo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductoResponse {
+
+    private Long id;
+
+    private String nombre;
+
+    private String descripcion;
+
+    private BigDecimal precio;
+
+    private Integer stock;
+}
